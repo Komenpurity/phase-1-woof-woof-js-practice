bb48b94bcecd55cb18c41c233891f0d151b3a7d8
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-04 10:43:01 +0300

    added a click event listener

--- src/index.js
+++ src/index.js
@@ -9,11 +9,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 })
 
-let dogBar = document.getElementById("dog-bar")
+
 
 function displayDogBar(data){ 
     data.forEach(dog => {
-        dogBar.innerHTML += `<span data-id = ${dog.id} > ${dog.name} </span>`  
-    });
-    console.log(dogBar) 
+      //  dogBar.innerHTML += `<span data-id = ${dog.id} > ${dog.name} </span>` 
+         
+        let dogBar = document.getElementById("dog-bar")
+        let span = document.createElement("span")
+            span.setAttribute("data-id", dog.id)
+            span.innerText = dog.name 
+            dogBar.appendChild(span)
+
+        span.addEventListener("click", () => {
+           // console.log("click")
+           displayDogInfo() 
+        })
+    });  
+    
+}
+
+
+function displayDogInfo(){
+    
 }
